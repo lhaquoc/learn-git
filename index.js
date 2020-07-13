7a3b08b99be184756a1565c932d8b196c779468c
lhaquoc <lhaquoc@gmail.com>
2020-07-13 16:24:24 +0700

    require Mouse, create a new mouse

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 var Dog = require('./Dog');
 var Cat = require('./Cat');
+var Mouse = require('./Mouse');
 
 var dog = new Dog('Ben');
 var cat = new Cat('Tom');
+var mouse = new Mouse('Jerry');
 dog.sayHello();
 dog.eat(cat);
 console.log(dog);
